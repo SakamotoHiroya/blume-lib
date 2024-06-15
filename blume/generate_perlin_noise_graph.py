b74362d8aad8dd9182e6b5dcbac9125da70b2d25
ito-tsubasa <tsubasa@progate.com>
2024-06-16 04:07:08 +0900

    a

--- blume/generate_perlin_noise_graph.py
+++ blume/generate_perlin_noise_graph.py
@@ -3,12 +3,12 @@ import triming as trim
 import output
 
 def generate_perlin_noise_graph(image_path):
-    gdi.line_drawing_image(image_path)
-    trim.crop_to_non_bg('line_drawing_output.jpg')
+    #gdi.line_drawing_image(image_path)
+    #trim.crop_to_non_bg('line_drawing_output.jpg')
     center_x, center_y = output.find_smallest_circle_center('cropped_image.jpg')
     distances = output.get_circle_distances('cropped_image.jpg', center_x, center_y)
-    distances = output.output(distances)
+    new_distances = output.output(distances)
 
-    return distances
+    return new_distances
 
-distances = generate_perlin_noise_graph('image/8.png')
+distances = generate_perlin_noise_graph("image/8.png")
